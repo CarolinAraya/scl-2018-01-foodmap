751f73fda00a8d0def97d0842a80fea65e2acc9c
DESKTOP-5H6PU6V\_caro <caro.frontend@gmail.com>
2018-07-25 18:53:03 -0400

    bootstrap

--- assets/app/googleAPI.js
+++ assets/app/googleAPI.js
@@ -35,24 +35,3 @@ var map, infoWindow;
         infoWindow.open(map);
       }
 
-function findMe() {
-    var output = document.getElementById('map');
-    // Verificar si soporta geolocalizacion
-    if (navigator.geolocation) {
-        output.innerHTML = "<p>Tu navegador soporta Geolocalizacion</p>";
-    } else {
-        output.innerHTML = "<p>Tu navegador no soporta Geolocalizacion</p>";
-    }
-    //Obtenemos latitud y longitud
-    function localizacion(posicion) {
-        var latitude = posicion.coords.latitude;
-        var longitude = posicion.coords.longitude;
-        var imgURL = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&size=600x300&markers=color:red%7C" + latitude + "," + longitude + "&key=AIzaSyDTQZkvA12MM0uPt4zxpc50EWHX5q9x0P4";
-        output.innerHTML = "<img src='" + imgURL + "'>";
-
-    }
-    function error() {
-        output.innerHTML = "<p>No se pudo obtener tu ubicación</p>";
-    }
-    navigator.geolocation.getCurrentPosition(localizacion, error);
-}
