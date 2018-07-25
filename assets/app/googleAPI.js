3c3eea8d6eac9360640c1bc2ebd2f823fcc8a382
DESKTOP-5H6PU6V\_caro <caro.frontend@gmail.com>
2018-07-25 16:35:36 -0400

    places API

--- assets/app/googleAPI.js
+++ assets/app/googleAPI.js
@@ -10,9 +10,9 @@ function findMe(){
     function localizacion(posicion){
         var latitude = posicion.coords.latitude;
         var longitude = posicion.coords.longitude;
-        var imgURL = "https://maps.googleapis.com/maps/api/staticmap?center="+latitude+","+longitude+"&size=600x300&markers=color:red%7C"+latitude+","+longitude+"&key=AIzaSyBsY2zEQuk3RMvpAqPwKNSjna6SafpLErU";
+        var imgURL = "maps.googleapis.com/maps/api/place/findplacefromtext/output?parameters"+latitude+","+longitude+"&size=600x300&markers=color:red%7C"+latitude+","+longitude+"&key=AIzaSyDTQZkvA12MM0uPt4zxpc50EWHX5q9x0P4";
         output.innerHTML ="<img src='"+imgURL+"'>";
-        
+        maps.googleapis.com/maps/api/place/findplacefromtext/output?parameters
     }
     function error(){
         output.innerHTML = "<p>No se pudo obtener tu ubicación</p>";
