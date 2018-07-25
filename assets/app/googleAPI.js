c4ee694e3e0c0cf4846333c85e67f7a7e5011f42
DESKTOP-5H6PU6V\_caro <caro.frontend@gmail.com>
2018-07-25 18:25:01 -0400

    simple map

--- assets/app/googleAPI.js
+++ assets/app/googleAPI.js
@@ -1,29 +1,29 @@
+var map;
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 43.5293101, lng: -5.6773233},
-      zoom: 13
+        center: { lat: -33.4724728, lng: -70.9100215 },
+        zoom: 8
     });
-  }
+}
 
-function findMe(){
+function findMe() {
     var output = document.getElementById('map');
     // Verificar si soporta geolocalizacion
     if (navigator.geolocation) {
         output.innerHTML = "<p>Tu navegador soporta Geolocalizacion</p>";
-    }else{
+    } else {
         output.innerHTML = "<p>Tu navegador no soporta Geolocalizacion</p>";
     }
     //Obtenemos latitud y longitud
-    function localizacion(posicion){
+    function localizacion(posicion) {
         var latitude = posicion.coords.latitude;
         var longitude = posicion.coords.longitude;
-        var imgURL = "maps.googleapis.com/maps/api/place/findplacefromtext/output?parametersnpm install geoip-lite"+latitude+","+longitude+"&size=600x300&markers=color:red%7C"+latitude+","+longitude+"&key=AIzaSyDTQZkvA12MM0uPt4zxpc50EWHX5q9x0P4";
-        output.innerHTML ="<img src='"+imgURL+"'>";
-        
+        var imgURL = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&size=600x300&markers=color:red%7C" + latitude + "," + longitude + "&key=AIzaSyDTQZkvA12MM0uPt4zxpc50EWHX5q9x0P4";
+        output.innerHTML = "<img src='" + imgURL + "'>";
+
     }
-    function error(){
+    function error() {
         output.innerHTML = "<p>No se pudo obtener tu ubicación</p>";
     }
-    //Método getCurrentPsition
-    navigator.geolocation.getCurrentPosition(localizacion,error);
+    navigator.geolocation.getCurrentPosition(localizacion, error);
 }
