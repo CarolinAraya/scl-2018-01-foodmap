25fb6b03f0a293aff6d8d56ab675a5bea6f2702d
DESKTOP-5H6PU6V\_caro <caro.frontend@gmail.com>
2018-07-25 11:39:34 -0400

    avance API

--- assets/app/googleAPI.js
+++ assets/app/googleAPI.js
@@ -10,12 +10,13 @@ function findMe(){
     function localizacion(posicion){
         var latitude = posicion.coords.latitude;
         var longitude = posicion.coords.longitude;
-        var imgURL = "https://maps.googleapis.com/maps/api/staticmap?center="+latitude+","+longitude+"&size=600x300&markers=color:red%7C"+latitude+","+longitude+"&key=YOUR_API_KEY";
+        var imgURL = "https://maps.googleapis.com/maps/api/staticmap?center="+latitude+","+longitude+"&size=600x300&markers=color:red%7C"+latitude+","+longitude+"&key=AIzaSyBsY2zEQuk3RMvpAqPwKNSjna6SafpLErU";
         output.innerHTML ="<img src='"+imgURL+"'>";
         
     }
     function error(){
         output.innerHTML = "<p>No se pudo obtener tu ubicación</p>";
     }
+    //Método getCurrentPsition
     navigator.geolocation.getCurrentPosition(localizacion,error);
 }
